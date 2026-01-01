a80cbe5308fc46e4e990168d30fa0b8559c1d176
Harits Chandra <haritschandradewata@gmail.com>
2026-01-01 22:09:11 +0700

    [Add] Penentuan Waktu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
-//toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
-
-//menu di click
-document.querySelector("#film-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
-
-// klik diluar sidebar agar sidebar hilang
-const film = document.querySelector("#film-menu");
-
-document.addEventListener("click", function (e) {
-  if (!film.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
-});
+function tampilkanJam() {
+  const now = new Date();
+  const waktu = now.toLocaleTimeString("id-ID");
+  document.getElementById("jam").innerText = waktu;
+}
+setInterval(tampilkanJam, 1000);
